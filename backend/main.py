9767b7789b280c1f40046e0a9f80d6b639fd5bc7
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2023-02-11 00:11:27 -0500

    attempt to push image into pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ def root():
 
     return render_template('index.html')
 
+@app.route('/pipeline', methods=['GET', 'POST'])
+def accept_input():
+    print(request.get_data())
+    if request.method == 'POST':
+        f = request.form['image']
+        if len(request.files) > 0:
+            return "Good"
+        return "No Good"
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
@@ -18,4 +28,4 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8081, debug=True)
