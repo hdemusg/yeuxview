803564fab28e42fd115a853e125e148361325d5f
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2023-02-11 11:52:01 -0500

    upload to cloud storage

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,46 @@
 from flask import Flask, render_template, request
 from PIL import Image
+from io import BytesIO
+import requests
+import os
+from google.cloud import storage
 
 app = Flask(__name__)
 
+TEST_FOLDER = '../static'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = TEST_FOLDER
+app.config['BUCKET_URL'] = "yeuxview.appspot.com"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
 
 @app.route('/')
 def root():
     # For the sake of example, use static information to inflate the template.
     # This will be replaced with real information in later steps.
+    my_img = {'image': open("fmicon.jpg", 'rb')}
+    r = requests.post('http://127.0.0.1:8081/pipeline', files=my_img)
+    filename=os.path.join(app.config['UPLOAD_FOLDER'], ' ', r.text)
+    return render_template('index.html', image=filename)
 
-    return render_template('index.html')
+@app.route('/pipeline-test', methods=['GET', 'POST'])
+def accept_input_test():
+    print('file: ', request.files["image"].filename)
+    file = request.files["image"]
+    img = Image.open(file.stream)
+    img.save('output.jpg')
+    return 'output.jpg'
 
 @app.route('/pipeline', methods=['GET', 'POST'])
 def accept_input():
-    print(request.get_data())
-    if request.method == 'POST':
-        f = request.form['image']
-        if len(request.files) > 0:
-            return "Good"
-        return "No Good"
+    file = request.files["image"]
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(app.config['BUCKET_URL'])
+    filename = file.filename
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)
+    
+    return 'saved successfully'
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
