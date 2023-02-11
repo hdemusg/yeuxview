c400018efffedb6a693e938553dff9ac273294bb
aiyer76 <aiyer76@gatech.edu>
2023-02-11 18:48:57 -0500

    Added data files and processing code.

--- backend/model.py
+++ backend/model.py
@@ -1,7 +1,10 @@
 import torch
+import os
 from torch import nn
 import torchvision
 from torchvision.models import resnet50, ResNet50_Weights
+from PIL import Image
+import glob
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,8 +13,9 @@ import requests
 
 class IrisNet(nn.Module):
 
-    def __init__(self, h, w):
+    def __init__(self):
         super(IrisNet, self).__init__()
+        self.epochs = 5
         self.model = resnet50(weights = ResNet50_Weights.DEFAULT)
         self.model.fc = nn.Sequential(
             nn.Dropout(0.5),
@@ -25,9 +29,37 @@ class IrisNet(nn.Module):
         return x
 
 
-iNet = IrisNet(250, 250)
-xOut = iNet.forward(x)
-criterion = nn.MultiLabelSoftMarginLoss()
-mLoss = criterion(xOut)
+# print(os.getcwd())
+image_dir = os.getcwd() + "/image_files"
+# print(image_dir)
+fileList = os.listdir(image_dir)
+leftFileList = []
+rightFileList = []
+for fname in fileList:
+    if "_left" in fname:
+        leftFileList.append(fname)
+    if "_right" in fname:
+        rightFileList.append(fname)
+# print(leftFileList)
+# print(rightFileList)
+# fileList.sort()
+# print(fileList)
+'''
 
-# print(iNet.model)
+image_list = []
+for fname in fileList:
+    impath = os.path.join(image_dir, fname)
+    im = Image.open(impath)
+    new_im = im.copy()
+    im.close()
+    image_list.append(new_im)
+print(len(image_list))
+'''
+iNet = IrisNet()
+# xOut = iNet.forward(x)
+# criterion = nn.MultiLabelSoftMarginLoss()
+# mLoss = criterion(xOut)
+for epoch in range(iNet.epochs):
+    pass
+    # xOut = iNet.forward(x)
+    # print(epoch)
